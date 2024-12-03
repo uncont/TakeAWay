b415a3844b6acea6a7320c5b78abd78b10778541
saber <3486188492@qq.com>
2024-12-03 16:16:12 +0800

    分支合并测试

--- src/stores/User.js
+++ src/stores/User.js
@@ -3,6 +3,7 @@ import { ref } from "vue";
 
 export const userStore = defineStore('user', ()=>{
     const count = ref(100);
+    const name = ref('')
     function add () {
       count.value++
     }
